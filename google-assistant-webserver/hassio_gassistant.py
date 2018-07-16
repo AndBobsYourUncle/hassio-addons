cfe243279d352b802ccaf64e7f996fd629a0ebab
Nicholas Page <npage85@gmail.com>
2018-07-15 17:53:43 -0700

    Updated the dockerfile and rebuilt using latest package versions.

--- google-assistant-webserver/hassio_gassistant.py
+++ google-assistant-webserver/hassio_gassistant.py
@@ -3,18 +3,25 @@ import json
 import sys
 from pathlib import Path
 
-import google.oauth2.credentials
+import os
+import logging
 
-from google.assistant.library import Assistant
-from google.assistant.library.event import EventType
-
+import click
 import google.auth.transport.grpc
 import google.auth.transport.requests
+import google.oauth2.credentials
 
 from google.assistant.embedded.v1alpha2 import (
     embedded_assistant_pb2,
     embedded_assistant_pb2_grpc
 )
+
+try:
+    from . import (
+        assistant_helpers
+    )
+except (SystemError, ImportError):
+    import assistant_helpers
 
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
@@ -24,12 +31,14 @@ api = Api(app)
 
 ASSISTANT_API_ENDPOINT = 'embeddedassistant.googleapis.com'
 DEFAULT_GRPC_DEADLINE = 60 * 3 + 5
+PLAYING = embedded_assistant_pb2.ScreenOutConfig.PLAYING
 
 class BroadcastMessage(Resource):
     def get(self):
         message = request.args.get('message', default = 'This is a test!')
         text_query = 'broadcast '+message
-        display_text = assistant.assist(text_query=text_query)
+        response_text, response_html = assistant.assist(text_query=text_query)
+        logging.debug(response_text)
         return {'status': 'OK'}
 
 api.add_resource(BroadcastMessage, '/broadcast_message')
@@ -37,30 +46,35 @@ api.add_resource(BroadcastMessage, '/broadcast_message')
 class Command(Resource):
     def get(self):
         message = request.args.get('message', default = 'This is a test!')
-        display_text = assistant.assist(text_query=message)
+        response_text, response_html = assistant.assist(text_query=message)
+        logging.debug(response_text)
         return {'status': 'OK'}
 
 api.add_resource(Command, '/command')
 
 
 class GoogleTextAssistant(object):
-    """Assistant that supports text based conversations.
+    """Sample Assistant that supports text based conversations.
 
     Args:
       language_code: language for the conversation.
       device_model_id: identifier of the device model.
       device_id: identifier of the registered device instance.
+      display: enable visual display of assistant response.
       channel: authorized gRPC channel for connection to the
         Google Assistant API.
       deadline_sec: gRPC deadline in seconds for Google Assistant API call.
     """
 
     def __init__(self, language_code, device_model_id, device_id,
-                 channel, deadline_sec):
+                 display, channel, deadline_sec):
         self.language_code = language_code
         self.device_model_id = device_model_id
         self.device_id = device_id
         self.conversation_state = None
+        # Force reset of first conversation.
+        self.is_new_conversation = True
+        self.display = display
         self.assistant = embedded_assistant_pb2_grpc.EmbeddedAssistantStub(
             channel
         )
@@ -77,37 +91,46 @@ class GoogleTextAssistant(object):
         """Send a text request to the Assistant and playback the response.
         """
         def iter_assist_requests():
-            dialog_state_in = embedded_assistant_pb2.DialogStateIn(
-                language_code=self.language_code,
-                conversation_state=b''
-            )
-            if self.conversation_state:
-                dialog_state_in.conversation_state = self.conversation_state
             config = embedded_assistant_pb2.AssistConfig(
                 audio_out_config=embedded_assistant_pb2.AudioOutConfig(
                     encoding='LINEAR16',
                     sample_rate_hertz=16000,
                     volume_percentage=0,
                 ),
-                dialog_state_in=dialog_state_in,
+                dialog_state_in=embedded_assistant_pb2.DialogStateIn(
+                    language_code=self.language_code,
+                    conversation_state=self.conversation_state,
+                    is_new_conversation=self.is_new_conversation,
+                ),
                 device_config=embedded_assistant_pb2.DeviceConfig(
                     device_id=self.device_id,
                     device_model_id=self.device_model_id,
                 ),
                 text_query=text_query,
             )
+            # Continue current conversation with later requests.
+            self.is_new_conversation = False
+            if self.display:
+                config.screen_out_config.screen_mode = PLAYING
             req = embedded_assistant_pb2.AssistRequest(config=config)
+            # This can be used to output the assistant request
+            # assistant_helpers.log_assist_request_without_audio(req)
             yield req
 
-        display_text = None
+        text_response = None
+        html_response = None
         for resp in self.assistant.Assist(iter_assist_requests(),
                                           self.deadline):
+            # This can be used to output the assistant response
+            # assistant_helpers.log_assist_response_without_audio(resp)
+            if resp.screen_out.data:
+                html_response = resp.screen_out.data
             if resp.dialog_state_out.conversation_state:
                 conversation_state = resp.dialog_state_out.conversation_state
                 self.conversation_state = conversation_state
             if resp.dialog_state_out.supplemental_display_text:
-                display_text = resp.dialog_state_out.supplemental_display_text
-        return display_text
+                text_response = resp.dialog_state_out.supplemental_display_text()
+        return text_response, html_response
 
 if __name__ == '__main__':
     global assistant
@@ -124,7 +147,9 @@ if __name__ == '__main__':
     grpc_channel = google.auth.transport.grpc.secure_authorized_channel(
         credentials, http_request, ASSISTANT_API_ENDPOINT)
 
+    logging.basicConfig(level=logging.DEBUG)
+
     # Create the text assistant
     assistant = GoogleTextAssistant('en-US', 'HA_GA', 'HA_GA_TEXT_SERVER',
-                             grpc_channel, DEFAULT_GRPC_DEADLINE)
+                             True, grpc_channel, DEFAULT_GRPC_DEADLINE)
     app.run(host='0.0.0.0')
