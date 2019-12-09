d5416e1e7e9cf05830ad469a0989a93654b400dc
Nicholas Page <npage85@gmail.com>
2019-12-08 18:37:44 -0800

    got it all working

--- usps-tracker/server.rb
+++ usps-tracker/server.rb
@@ -18,18 +18,19 @@ require "googleauth"
 require "googleauth/stores/file_token_store"
 require "fileutils"
 
-file = File.read(OPTIONS_FILE)
-options_json = JSON.parse(file)
-
 OPTIONS_FILE = if Sinatra::Base.production?
   '/data/options.json'
 else
   'options.json'
 end
 
-OOB_URI = "https://#{ENV['HOST']}".freeze
-APPLICATION_NAME = options_json['project_id'].freeze
-CREDENTIALS_PATH = options_json['client_secrets'].freeze
+file = File.read(OPTIONS_FILE)
+OPTIONS = JSON.parse(file)
+
+OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+
+APPLICATION_NAME = OPTIONS['project_id'].freeze
+CREDENTIALS_PATH = OPTIONS['client_secrets'].freeze
 # The file token.yaml stores the user's access and refresh tokens, and is
 # created automatically when the authorization flow completes for the first
 # time.
@@ -41,21 +42,17 @@ end
 
 SCOPE = Google::Apis::GmailV1::AUTH_GMAIL_READONLY
 
-# service = Google::Apis::GmailV1::GmailService.new
-# service.client_options.application_name = APPLICATION_NAME
-# service.authorization = authorize
-
 def authenticate_google(code: '')
   client_id = Google::Auth::ClientId.from_file CREDENTIALS_PATH
   token_store = Google::Auth::Stores::FileTokenStore.new file: TOKEN_PATH
-  authorizer = Google::Auth::UserAuthorizer.new client_id, SCOPE, token_store, '/authorize'
+  authorizer = Google::Auth::UserAuthorizer.new client_id, SCOPE, token_store
   user_id = "default"
   credentials = authorizer.get_credentials user_id
   if credentials.nil? && code.empty?
     url = authorizer.get_authorization_url base_url: OOB_URI
     puts "Open the following URL in the browser and enter the " \
-         "resulting code after authorization:\n" + url
-  else
+         "resulting code after authorization into the WebUI of this addon:\n" + url
+  elsif credentials.nil?
     credentials = authorizer.get_and_store_credentials_from_code(
       user_id: user_id, code: code, base_url: OOB_URI
     )
@@ -74,16 +71,30 @@ use(Rack::Conneg) { |conneg|
   conneg.provide([:json])
 }
 
-authenticate_google
+GOOGLE_CREDENTIALS = authenticate_google
 
-# get '/authenticate' do
-#   erb :authenticate
-# end
+get '/authenticate' do
+  erb :authenticate
+end
 
 post '/authenticate' do
-  puts params.to_s
+  authenticate_google(code: params['code'])
+
+  erb :authenticated
 end
 
 get '/test' do
+  service = Google::Apis::GmailV1::GmailService.new
+  service.client_options.application_name = APPLICATION_NAME
+  service.authorization = GOOGLE_CREDENTIALS
+
+  messages = service.list_user_messages("me", q: "from:auto-reply@usps.com after:1575750604").messages
+
+  message = service.get_user_message("me", messages.first.id)
+
+  subject = message.payload.headers.find { |header| header.name == 'Subject'}.value
+
+  puts subject
+
   [200, {"status": CREDENTIALS_PATH}.to_json]
 end
